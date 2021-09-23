0bdf4f6d04fe8550b3168454566b6ca83239cd6c
Vadim Abzalov <st067798@student.spbu.ru>
2021-09-23 17:56:19 +0300

    ✨ 🚀 ✨ [Issue #16] Add `weights`

--- genegram/__main__.py
+++ genegram/__main__.py
@@ -2,7 +2,7 @@ import tempfile
 from argparse import ArgumentParser
 from pathlib import Path
 
-from genegram import seq_fasta_to_pictures, predict
+from genegram import seq_fasta_to_pictures, predict, ROOT
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Genegram")
@@ -15,6 +15,19 @@ if __name__ == "__main__":
         type=str,
         help="Path to the folder where the predictions will be saved",
     )
+    parser.add_argument(
+        "--model",
+        required=False,
+        type=str,
+        choices=["main", "mps", "pks"],
+        default="main",
+        help=(
+            "Type of the model to be used:\n"
+            "main -- The default model, the best on average\n"
+            "mps -- Multiplet prediction model\n"
+            "pks -- Pseudoknots prediction model\n"
+        ),
+    )
 
     args = parser.parse_args()
 
@@ -22,4 +35,4 @@ if __name__ == "__main__":
 
     with tempfile.TemporaryDirectory() as tmp_dir:
         seq_fasta_to_pictures(seq_fasta, tmp_dir)
-        predict(tmp_dir, args.out)
+        predict(tmp_dir, args.out, ROOT / "weights" / f"{args.model}.h5")
